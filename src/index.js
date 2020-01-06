0cca0371d90b737214efc48597b492c438cbd49c
Sergio Velazquez <sergio_velazquez@outlook.com>
2020-01-06 16:51:37 -0700

    add delete interface

---
+++ src/index.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const app = new express();
+let users = [];
+
+app.get('/', function(req, res) {
+    res.send('Hello World!')
+}); 
+
+app.get('/users', (req, res) => {  
+  res.json(users)
+
+});
+
+app.get('/user/:id', (req, res) => {
+  const user = users.find(user => user.id == req.params.id);
+  res.json(user);
+});
+
+app.post('/user', (req, res) => {
+  users.push({id:0})
+  res.json(users);
+});
+
+app.delete('/user/:id', (req, res) => {
+  if(users.length > 0){
+      users = [];
+      res.status(202).json({
+          ok: true
+      })
+  } else {
+      res.status(204).json({
+          ok: true
+      });
+  }
+})
+
+app.listen(3000,()=> {
+  console.log('Listen on port 3000');
+})

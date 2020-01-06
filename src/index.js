3b6af11ca65a5a7cabab080123e89442f08c2061
Sergio Velazquez <sergio_velazquez@outlook.com>
2020-01-06 16:04:54 -0700

    add user interface

--- src/index.js
+++ src/index.js
@@ -3,11 +3,26 @@ const express = require('express');
 
 const app = new express();
 
-
-
 app.get('/', function(req, res) {
     res.send('Hello World!')
 }) 
+
+app.get('/users',function(req,res){
+	res.json({
+		success: true,
+		message: 'successfully got users. Nice!',
+		users: mockUserData
+	})
+})
+
+app.get('/users/:id',function(req,res){
+	console.log(req.params.id)
+	res.json({
+		success: true,
+		message: 'got one user',
+		user: req.params.id
+	})
+})
 
 app.listen(3000,()=> {
   console.log('Listen on port 3000');

3d994047e6a7bebb120bff587c79689d79a5f1b7
Sergio Velazquez <sergio_velazquez@outlook.com>
2020-01-06 16:25:06 -0700

    add user interface

--- src/index.js
+++ src/index.js
@@ -1,28 +1,21 @@
 // content of index.js
 const express = require('express');
-
 const app = new express();
+let users = [];
 
 app.get('/', function(req, res) {
     res.send('Hello World!')
-}) 
+}); 
 
-app.get('/users',function(req,res){
-	res.json({
-		success: true,
-		message: 'successfully got users. Nice!',
-		users: mockUserData
-	})
-})
+app.get('/users', (req, res) => {  
+  res.json(users)
 
-app.get('/users/:id',function(req,res){
-	console.log(req.params.id)
-	res.json({
-		success: true,
-		message: 'got one user',
-		user: req.params.id
-	})
-})
+});
+
+app.get('/user/:id', (req, res) => {
+  const user = users.find(user => user.id == req.params.id);
+  res.json(user);
+});
 
 app.listen(3000,()=> {
   console.log('Listen on port 3000');
